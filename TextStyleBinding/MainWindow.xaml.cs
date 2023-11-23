fd223ad41376b6652c216dfebf94bdc19b925082
Aleksandr Kladov <119361895+kladoov@users.noreply.github.com>
2023-11-23 19:18:11 +0100

    C#/WPF
    Look the screenshot (screen.png) to view my interface.
    El programa tiene una interfaz creada por mi mismo a base de código XAML apoyado con C# para eventos.
    Los componentes que se ven, sirven para cambiar el formato de la letra de la (label).
    - Slider: para modificar el tamaño.
    - ListColores: para cambiar el color del texto.
    - ListFontFamily: para cambiar el formato de la letra.
    - TextBox: para indicar el tamaño de la label.
    - Buttons: para alternar tamaños de menor a mayor.

---
+++ TextStyleBinding/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextoEstiloBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPequeño_Click(object sender, RoutedEventArgs e)
+        {
+            this.sldProgreso.Value = 5;
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            this.sldProgreso.Value = 20;
+        }
+
+        private void btnGrande_Click(object sender, RoutedEventArgs e)
+        {
+            this.sldProgreso.Value = 40;
+        }
+    }
+}
